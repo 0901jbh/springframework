44702b6308a353226936b4a5aeda5cfd66380eb6
jbh <0901jbh@naver.com>
2022-11-10 17:47:37 +0900

    til vue

--- step06_mvc_rest_boot/src/main/java/com/ssafy/empapp/model/dto/Dept.java
+++ step06_mvc_rest_boot/src/main/java/com/ssafy/empapp/model/dto/Dept.java
@@ -5,13 +5,9 @@ import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
 
 @Builder
 @Data
